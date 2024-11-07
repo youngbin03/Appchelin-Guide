bb94cde1eb69a5e0461d55a90fc4f71bfc7a1a48
jaehun <jaehun330@gmail.com>
2024-11-07 22:52:11 +0900

    jaehun's commit

--- Week 01/test/03.dart
+++ Week 01/test/03.dart
@@ -2,8 +2,8 @@
 // 데이터 타입을 수정해 에러를 해결해보세요.
 void main() {
   List<String> aListOfStrings = ['one', 'two', 'three'];
-  List<String> aNullableListOfStrings;
-  List<String> aListOfNullableStrings = ['one', null, 'three'];
+  String? aNullableListOfStrings;
+  List<String?> aListOfNullableStrings = ['one', null, 'three'];
 
   print('aListOfStrings is $aListOfStrings.');
   print('aNullableListOfStrings is $aNullableListOfStrings.');
--- Week 01/test/04.dart
+++ Week 01/test/04.dart
@@ -1,7 +1,7 @@
 // 아래 함수에서 str이 null인 경우 0을 반환하는
 // if문을 추가해 에러를 해결해 주세요.
 int getLength(String? str) {
-  // null 검사 코드를 여기에 작성해주세요.
+  if (str == null) return 0;
 
   return str.length;
 }
